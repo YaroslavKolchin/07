3be1ce72dd59f5b42c258af36184d3869d3eff5e
YaroslavKolchin <39592718+YaroslavKolchin@users.noreply.github.com>
2019-03-07 11:30:13 +0300

    Add files via upload

---
+++ 07/tank/Tank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    class Tank
+    {
+        protected string name;
+        protected int ammo;
+        protected int armor;
+        protected int mobility;
+        public Tank(int ammo, int armor, int mobility, string name)
+        {
+            this.ammo = ammo;
+            this.armor = armor;
+            this.mobility = mobility;
+            this.name = name;
+        }
+        public Tank()
+        {
+            ammo = 0;
+            armor = 0;
+            mobility = 0;
+            name = "null";
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name - {0}", name);
+            Console.WriteLine("Ammunation = {0} Armor = {0} Mobility = {0}", ammo, armor, mobility);
+        }
+
+        public static Tank operator *(Tank T34, Tank pantera)
+        {
+            if (T34.ammo > pantera.ammo && T34.armor > pantera.armor)
+                return T34;
+
+            if (T34.ammo > pantera.ammo && T34.mobility > pantera.mobility)
+                return T34;
+
+            if (T34.armor > pantera.armor && T34.mobility > pantera.mobility)
+                return T34;
+            else
+                return pantera;
+        }
+    }
+}
